2a63e99ac74125e580a9ee475f6adf4ebbff7d15
megansin <megan_sin@student.wayland.k12.ma.us>
2022-11-24 15:31:44 -0500

    Initial commit

---
+++ TicTacToeGame.py
@@ -0,0 +1,27 @@
+from TicTacToeClass import *
+
+
+def restart():
+    print("Would you like to run the game again? (yes/no)")
+    again_question = input()
+    if again_question.lower() == "yes":
+        print("")
+        main()
+    elif again_question.lower() == "no":
+        print("")
+        print("Thank You for using our App!")
+    else:
+        print("Invalid input, please try again.")
+        print("")
+        restart()
+
+
+def main():
+    s = input("Please enter the size of the game: ")
+    size = int(s)
+    play_game(TicTacToe(size, size, size), {'X': random_player, 'O': player(alphabeta_search)}, verbose=True)
+    restart()
+
+
+if __name__ == "__main__":
+    main()
